48094d834da8a29c5583a44a0b26c57a0e5a100a
alifmusthofa354 <muhammad.alif.musthofa354@gmail.com>
2026-01-05 15:00:54 +0700

    game init

---
+++ android/src/main/java/com/ae2rm/game/android/AndroidLauncher.java
@@ -0,0 +1,18 @@
+package com.ae2rm.game.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.ae2rm.game.AE2Game;
+
+/** Launches the Android application. */
+public class AndroidLauncher extends AndroidApplication {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
+        configuration.useImmersiveMode = true; // Recommended, but not required.
+        initialize(new AE2Game(), configuration);
+    }
+}
---
+++ core/src/main/java/com/ae2rm/game/AE2Game.java
@@ -0,0 +1,11 @@
+package com.ae2rm.game;
+
+import com.badlogic.gdx.Game;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class AE2Game extends Game {
+    @Override
+    public void create() {
+        setScreen(new FirstScreen());
+    }
+}
